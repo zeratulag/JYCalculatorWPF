e77de3bde915e2a790b86bc7a87188fc9467877c
Benny Laffin <59944417+zeratulag@users.noreply.github.com>
2024-09-12 23:55:45 +0800

    bug fix

---
+++ JX3CalculatorShared/JX3CalculatorShared/Src/Class/KBuffEntity.cs
@@ -0,0 +1,15 @@
+﻿using JYCalculator.JX3CalculatorShared.Src;
+
+namespace JX3CalculatorShared.Class
+{
+    public class KBuffEntity
+    {
+        // 表示游戏内Buff实际数值效果的类
+        public string Name;
+        public string BuffID;
+        public double BuffCover;
+        public double Stack;
+        public KAttributeEntry[] KAttributeEntries;
+
+    }
+}
---
+++ JX3CalculatorShared/JX3CalculatorShared/Src/KAttributeEntry.cs
@@ -0,0 +1,13 @@
+﻿using JX3CalculatorShared.Class;
+
+namespace JYCalculator.JX3CalculatorShared.Src
+{
+    public class KAttributeEntry : KAttributeEntryBase
+    {
+        public double Value { get; set; }
+        public KAttributeEntry(string modifyType, double value) : base(modifyType)
+        {
+            Value = value;
+        }
+    }
+}
